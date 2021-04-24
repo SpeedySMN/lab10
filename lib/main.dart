cb21a7fa6bc481076be65a393d2417f71cae795b
Samad <60344452+Kebrima@users.noreply.github.com>
2021-04-24 21:36:59 +0600

    My first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:labwork_10/page_one.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Register Form Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: PageOne(),
+    );
+  }
+}
